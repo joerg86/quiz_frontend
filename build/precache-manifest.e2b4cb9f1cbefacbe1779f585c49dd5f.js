fa20269ab7a4ae06c8d93f14a5616c94dc260c19
Joerg Sawatzki <sawatzki.joerg@gmail.com>
2020-05-18 18:35:16 +0200

    Adding build for convenience.

---
+++ build/precache-manifest.e2b4cb9f1cbefacbe1779f585c49dd5f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca37c274b69e3f2db813668d65ed4ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d8e45b1088b02494b81",
+    "url": "/static/css/main.b8d82d23.chunk.css"
+  },
+  {
+    "revision": "98f29dad43c9ec955c39",
+    "url": "/static/js/2.6c5156e4.chunk.js"
+  },
+  {
+    "revision": "4b5437abc16a19d81789a84fc8b12c88",
+    "url": "/static/js/2.6c5156e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8e45b1088b02494b81",
+    "url": "/static/js/main.caae858e.chunk.js"
+  },
+  {
+    "revision": "c273b041dd2a86d167fd",
+    "url": "/static/js/runtime-main.37f1d1b9.js"
+  },
+  {
+    "revision": "ad14f417464d1a2b3d8773862a52a54a",
+    "url": "/static/media/answer.ad14f417.jpg"
+  },
+  {
+    "revision": "71059c267d26ebbcc1e0a5c800024cc8",
+    "url": "/static/media/delete.71059c26.jpg"
+  },
+  {
+    "revision": "f7026e52866a7274e4ee275517cd151b",
+    "url": "/static/media/edit.f7026e52.jpg"
+  },
+  {
+    "revision": "46d4f94082b18a14d194518b22c11bbf",
+    "url": "/static/media/iubh.46d4f940.png"
+  },
+  {
+    "revision": "d50fabee466321809be4a7c634e219f5",
+    "url": "/static/media/kb.d50fabee.jpg"
+  },
+  {
+    "revision": "faf2bcfcf01b1611a783c37417a1c3ae",
+    "url": "/static/media/login.faf2bcfc.jpg"
+  },
+  {
+    "revision": "b3e38b7e66fed889aa03a07b47e8426f",
+    "url": "/static/media/play.b3e38b7e.jpg"
+  },
+  {
+    "revision": "6c4dd8df4a2f9c16cc12df41e742d4f5",
+    "url": "/static/media/question.6c4dd8df.jpg"
+  },
+  {
+    "revision": "5821ed375ca6639a427483902957852f",
+    "url": "/static/media/scoring.5821ed37.jpg"
+  },
+  {
+    "revision": "7dcb19430c28d86eb8dd74ee96c1cda0",
+    "url": "/static/media/seascape.7dcb1943.jpg"
+  },
+  {
+    "revision": "ee4f8d13104d506bb4baad55e9710eff",
+    "url": "/static/media/team.ee4f8d13.jpg"
+  },
+  {
+    "revision": "6d414af420551deaa0ae41a69e14c306",
+    "url": "/static/media/wait1.6d414af4.jpg"
+  },
+  {
+    "revision": "833fd2d4fa0398d1de0d4964cbd72c43",
+    "url": "/static/media/wait2.833fd2d4.jpg"
+  }
+]);
